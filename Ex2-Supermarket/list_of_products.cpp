22fa685952c64880fad538ee7826613df5652b0f
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-15 16:48:34 +0300

    add cpp file of list of products with documentation

---
+++ Ex2-Supermarket/list_of_products.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: list_of_products.cpp
+Author: Liri (15/4/2020)
+Purpose: This file contain the funcions that add node to list of products,
+		 discount the price of the products and print the products
+\********************************************************/
