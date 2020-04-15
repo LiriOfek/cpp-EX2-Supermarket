b5589437803fe4567e4a6d9b6d3357508530ef75
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-15 17:05:36 +0300

    add main file with documentation

---
+++ Ex2-Supermarket/main.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (15/4/2020)
+Purpose: This file test functions in list_of_products.cpp,
+		 make a list of products, discount their price, and print the
+		 results
+\********************************************************/
