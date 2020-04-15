83e4a35e718f4fd09ab983d271e2d63670865d5d
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-15 16:45:45 +0300

    add header file of list of products with documentation

---
+++ Ex2-Supermarket/list_of_products.h
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: list_of_products.h
+Author: Liri (15/4/2020)
+Purpose: This file contain the structure of list of products,
+	     and declaration of functions that add node to list of
+		 products, discount the price of the products and print
+		 the products
+\********************************************************/
