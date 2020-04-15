8d5049e332608b21741696530964b308ecd712bd
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-15 16:34:24 +0300

    add header file of product with documentation

---
+++ Ex2-Supermarket/product.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: product.h
+Author: Liri (15/4/2020)
+Purpose: This file contain the structure of the producet, 
+		 and the enum structure of categories of products
+\********************************************************/
