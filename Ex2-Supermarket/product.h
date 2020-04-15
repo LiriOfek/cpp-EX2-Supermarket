e4238b6c1b49a235ff5283f8ae056065f717f057
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-15 16:35:38 +0300

    add enum structure of product categories

--- Ex2-Supermarket/product.h
+++ Ex2-Supermarket/product.h
@@ -4,3 +4,14 @@ Author: Liri (15/4/2020)
 Purpose: This file contain the structure of the producet, 
 		 and the enum structure of categories of products
 \********************************************************/
+
+#pragma once
+
+/*enum structute of categories of products*/
+typedef enum {
+	BREAD,
+	DRINK,
+	VEGETABLES,
+	FRUITS,
+	MEAT
+}CATEGORY;
