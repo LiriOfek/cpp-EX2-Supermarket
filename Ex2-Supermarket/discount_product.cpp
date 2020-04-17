13719cff0d5608f7fc4d0bc68a8dde9be85d1d30
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-17 10:15:32 +0300

    add files that discount products according to the categories and add documentation of the files

---
+++ Ex2-Supermarket/discount_product.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: discount_product.cpp
+Author: Liri (17/4/2020)
+Purpose: This file discount each product according to the category
+		 it belongs to
+\********************************************************/
---
+++ Ex2-Supermarket/discount_product.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: discount_product.h
+Author: Liri (17/4/2020)
+Purpose: This header file contain the declaration of function that
+		 discount the price of each product in product vector according
+		 to the category it belongs to
+\********************************************************/
