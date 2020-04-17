be6bceff54600701b5e20c9613fefce367e1282a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-17 10:17:32 +0300

    add function that discount the price of each product in the vector of products according to the category it belongs to

--- Ex2-Supermarket/discount_product.h
+++ Ex2-Supermarket/discount_product.h
@@ -2,6 +2,33 @@
 File Name: discount_product.h
 Author: Liri (17/4/2020)
 Purpose: This header file contain the declaration of function that
-		 discount the price of each product in product vector according
-		 to the category it belongs to
+discount the price of each product in product vector according
+to the category it belongs to
 \********************************************************/
+
+#pragma once
+
+#include "product.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+const char* const PRINT_COST = " costs ";
+const char* const DOLLAR = "$";
+const unsigned int HUNDRED = 100;
+
+void discount_product(std::vector<Product> &products_vector,
+	std::map<CATEGORY, double> discount_category_map);
+/**
+* @brief  discount each product according to the category it belongs to
+* @param  INOUT std::vector<Product> &products_vector - the vector
+*		  of products
+*		  IN std::map<CATEGORY, double> discount_category_map - the map of
+*		  the discount that corresponding to the categoryS
+* @notes  the vector of products changed such that each price of product
+*		  discount according to its category
+* @return this function has no return value
+* @author  Liri
+*/
